81e699179943a1432235e0a8e719b317f5f8a6c5
Rémi Bardon <remi@remibardon.name>
2024-11-09 12:43:01 +0100

    ✨ Use `node-fetch` to fetch the web page for real

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
+import fetch from "node-fetch";
+
 const FORM_URL =
   "https://ipag.osug.fr/french/grand-public/soirees-d-observation-du-ciel-en-hiver/formulaire-inscription-coupole-ipag/";
 
-/**
- * Fetches a web page and returns its content.
- * @param {string} url - The web page URL.
- * @returns {string} The web page content.
- */
-function fetch(url) {
-  console.log(`Fetching '${url}'…`);
-  return "todo";
-}
-
-const content = fetch(FORM_URL);
-console.log(`The page contains '${content}'`);
+console.info(`Fetching '${FORM_URL}'…`);
+const response = await fetch(FORM_URL);
+const content = await response.text();
+console.debug(`The page contains '${content.slice(0, 256)}[...]'`);
