a70d452e9e4f44e9021a33807053c9432ae15c49
Rémi Bardon <remi@remibardon.name>
2024-11-09 12:25:18 +0100

    ✨ Create dummy `fetch` function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const FORM_URL =
+  "https://ipag.osug.fr/french/grand-public/soirees-d-observation-du-ciel-en-hiver/formulaire-inscription-coupole-ipag/";
+
+/**
+ * Fetches a web page and returns its content.
+ * @param {string} url - The web page URL.
+ * @returns {string} The web page content.
+ */
+function fetch(url) {
+  console.log(`Fetching '${url}'…`);
+  return "todo";
+}
+
+const content = fetch(FORM_URL);
+console.log(`The page contains '${content}'`);
