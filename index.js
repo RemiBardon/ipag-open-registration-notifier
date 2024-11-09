1bac9cb8b953365c27707cabbd2872e91289b5da
Rémi Bardon <remi@remibardon.name>
2024-11-09 13:01:28 +0100

    ✨ Count available slots using `jsdom`

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
 
 const FORM_URL =
   "https://ipag.osug.fr/french/grand-public/soirees-d-observation-du-ciel-en-hiver/formulaire-inscription-coupole-ipag/";
 const NOT_YET_OPEN_MESSAGE =
   "Les réservations  pour la saison 2024-2025 ouvriront sur cette page à la mi Novembre 2024";
+const SELECT_ELEMENT_ID = "#champ_selection_1";
 
 console.info(`Fetching '${FORM_URL}'…`);
 const response = await fetch(FORM_URL);
@@ -12,3 +14,9 @@ console.debug(`The page contains '${content.slice(0, 256)}[...]'`);
 
 const is_open = !content.includes(NOT_YET_OPEN_MESSAGE);
 console.log(`Registrations are open: ${is_open}`);
+
+const dom = new JSDOM(content);
+const document = dom.window.document;
+
+const slots = document.querySelectorAll(`${SELECT_ELEMENT_ID} option`);
+console.log(`There are ${slots.length} slots available.`);
